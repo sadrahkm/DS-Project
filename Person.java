f4787f45ab464db54ffce3ac38842b28c49c669d
DS-project <mohammadi1380152@gmail.com>
2019-12-25 13:10:08 +0330

    add method make

--- Person.java
+++ Person.java
@@ -8,14 +8,29 @@ public class Person extends Node {
     String birthPlace;
     String workPlace;
     static CSVReader test = new CSVReader();
-    static ArrayList<String[]> data = test.get("C:\\Users\\Sadra Hakim\\Desktop\\data\\people.csv");
-
+    static ArrayList<String[]> data = test.get("E:\\DS project\\src\\data\\people.csv");
+    static  Dictionary<String,String> dict = new Hashtable<>();
+    public static void make() {
+        for (int i = 0; i < data.size(); i++)
+            for (int j = 0; j < data.get(i).length; j++)
+                dict.put(data.get(i)[2],data.get(i)[j]);
+    }
     public static void showData(){
         for (int i = 0; i < data.size(); i++) {
             for (int j = 0; j < data.get(i).length; j++) {
                 System.out.print(data.get(i)[j] + ",");
             }
             System.out.println();
+        }
+    }
+    public void sazmanipeople(String num){
+        ArrayList<String> pesonjob=new ArrayList<>();
+        pesonjob.add()
+        for (int i = 0; i < data.size(); i++)
+            for (int j = 0; j < pesonjob.size(); j++)
+                if (data.get(i)[5].equals(pesonjob.get(j))){
+                    continue;
+            }
         }
     }
 }
