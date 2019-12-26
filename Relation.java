f5cd368fe5e12ff06dc2778ef39c3d1791f0bac4
DS-project <mohammadi1380152@gmail.com>
2019-12-26 16:54:21 +0330

    Second Faz// complete Relation and  Tarakonesh Yal

--- Relation.java
+++ Relation.java
@@ -1,7 +1,34 @@
+
+
+
+
+
+
+
+import java.util.ArrayList;
+import java.util.Dictionary;
 import java.util.Enumeration;
+import java.util.Hashtable;
 
 public class Relation extends Yal {
     String nesbat;
     String nesbatTime;
-    String key;
+    static Dictionary<String, Relation> dict = new Hashtable<>();
+
+    Relation(String to, String from, String nesbat, String nesbatTime){
+        this.to= to ;
+        this.from= from;
+        this.nesbat= nesbat;
+        this.nesbat= nesbatTime;
+        this.key=from+to;
+    }
+
+    public static void make(String path) {
+        CSVReader read = new CSVReader();
+        ArrayList<String[]> data = read.get(path);
+        for (int i = 0; i < data.size(); i++)
+            dict.put( data.get(i)[0]+data.get(i)[1] , new Relation(data.get(i)[0], data.get(i)[1], data.get(i)[2], data.get(i)[3]));
+    }
+
+
 }
--- Tarakonesh.java
+++ Tarakonesh.java
@@ -5,4 +5,20 @@ public class Tarakonesh extends Yal {
     String number;
     String price;
     String key = number;
+    static Dictionary<String, Relation> dict = new Hashtable<>();
+
+    Tarakonesh(String time, String number, String price){
+        this.time= time;
+        this.number= number;
+        this.price= price;
+        this.key= number;
+    }
+
+    public static void make(String path) {
+        CSVReader read = new CSVReader();
+        ArrayList<String[]> data = read.get(path);
+        for (int i = 0; i < data.size(); i++)
+            dict.put( data.get(i)[1] , new Relation(data.get(i)[0], data.get(i)[1], data.get(i)[2], data.get(i)[3]));
+    }
+
 }
