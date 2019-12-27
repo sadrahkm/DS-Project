610e5442b6ee2f8687259c8174b07fa3cfcc368b
Sadra Hakim <sadrahkm0000@gmail.com>
2019-12-27 19:44:10 +0330

    Add Show Method Relationship & Fix make method

--- Relation.java
+++ Relation.java
@@ -1,10 +1,6 @@
 
 
 
-
-
-
-
 import java.util.ArrayList;
 import java.util.Dictionary;
 import java.util.Enumeration;
@@ -15,20 +11,31 @@ public class Relation extends Yal {
     String nesbatTime;
     static Dictionary<String, Relation> dict = new Hashtable<>();
 
-    Relation(String to, String from, String nesbat, String nesbatTime){
-        this.to= to ;
-        this.from= from;
-        this.nesbat= nesbat;
-        this.nesbat= nesbatTime;
-        this.key=from+to;
+    Relation(Person to, Person from, String nesbat, String nesbatTime) {
+        this.to = to;
+        this.from = from;
+        this.nesbat = nesbat;
+        this.nesbatTime = nesbatTime;
+//        this.key = from.code + to.code;
     }
 
     public static void make(String path) {
         CSVReader read = new CSVReader();
         ArrayList<String[]> data = read.get(path);
-        for (int i = 0; i < data.size(); i++)
-            dict.put( data.get(i)[0]+data.get(i)[1] , new Relation(data.get(i)[0], data.get(i)[1], data.get(i)[2], data.get(i)[3]));
+        Person personFrom;
+        Person personTo;
+        for (int i = 0; i < data.size(); i++) {
+            personTo = Person.dict.get(data.get(i)[0]);
+            personFrom = Person.dict.get(data.get(i)[1]);
+            dict.put(data.get(i)[0] + data.get(i)[1], new Relation(personTo, personFrom, data.get(i)[2], data.get(i)[3]));
+        }
     }
 
-
+    public static void showData() {
+        Enumeration e = dict.elements();
+        while (e.hasMoreElements()) {
+            Relation temp = (Relation) e.nextElement();
+            System.out.println(((Person) temp.to).name + " , " + ((Person) temp.from).name + " , " + temp.nesbat + " , " + temp.nesbatTime);
+        }
+    }
 }
