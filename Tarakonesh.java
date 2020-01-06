5ce32e7d9c4812392ba1bf1ef6f9ff9476116a84
Sadra Hakim <sadrahkm0000@gmail.com>
2020-01-06 23:46:08 +0330

    Chnage to&from Type to Object

--- Tarakonesh.java
+++ Tarakonesh.java
@@ -6,20 +6,25 @@ public class Tarakonesh extends Yal {
     String price;
     static Dictionary<String, Tarakonesh> dict = new Hashtable<>();
 
-    Tarakonesh(String from, String to ,  String number, String time, String price){
-        this.from= from;
+    Tarakonesh(BankAccount from, BankAccount to, String number, String time, String price) {
+        this.from = from;
         this.to = to;
-        this.number= number;
-        this.time= time;
-        this.price= price;
-        this.key= number;
+        this.number = number;
+        this.time = time;
+        this.price = price;
+        this.key = number;
     }
 
     public static void make(String path) {
         CSVReader read = new CSVReader();
         ArrayList<String[]> data = read.get(path);
-        for (int i = 0; i < data.size(); i++)
-            dict.put( data.get(i)[2] , new Tarakonesh(data.get(i)[0], data.get(i)[1], data.get(i)[2],data.get(i)[3],data.get(i)[4]));
+        BankAccount accountFrom;
+        BankAccount accountTo;
+        for (int i = 0; i < data.size(); i++) {
+            accountFrom = BankAccount.dict.get(data.get(i)[0]);
+            accountTo = BankAccount.dict.get(data.get(i)[1]);
+            dict.put(data.get(i)[2], new Tarakonesh(accountFrom, accountTo, data.get(i)[2], data.get(i)[3], data.get(i)[4]));
+        }
     }
 
 }
